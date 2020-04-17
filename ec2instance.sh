361c3d2a2b2b4231f554c09cb870d1640284421a
rpnegro <rpnegro@gmail.com>
2020-04-17 10:44:27 -0500

    Update ec2instance.sh

--- ec2instance.sh
+++ ec2instance.sh
@@ -12,3 +12,6 @@ chkconfig httpd on
 usermod -a -G apache ec2-user
 sudo chown -R ec2-user:apache /var/www
 sudo chmod 2775 /var/www
+echo "fs-71f7fef1.efs.us-east-1.amazonaws.com:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
+mount -a
+systemctl restart httpd
